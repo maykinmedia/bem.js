14d957775e42f7390b3bc5f88aea55c9e5f63246
Sven van de Scheur <svenvandescheur@gmail.com>
2018-03-02 15:05:59 +0100

    Fix a bug which could cause a partially matched modifier to incorrectly be removed.

--- src/bem.js
+++ src/bem.js
@@ -145,8 +145,12 @@ class BEM {
             return;
         }
 
-        let regex = new RegExp(`[^^\\s]+?--${modifier}\\s?`, 'g');  // Regex matching all class names containing "--" + modifier
-        node.className = node.className.replace(regex, '').trim();
+        let regex = new RegExp(`--${modifier}$`, 'g');
+        node.classList.forEach(className => {
+           if (className.match(regex)) {
+               node.classList.remove(className);
+           }
+        });
     }
 
 
