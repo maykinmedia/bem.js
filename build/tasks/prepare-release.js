281c68ee98a565e4e7a3b23a92d419edf8393fcc
Sven van de Scheur <svenvandescheur@gmail.com>
2018-03-02 15:09:57 +0100

    Forgot to add "jsdoc" task to prepare-release task.

--- build/tasks/prepare-release.js
+++ build/tasks/prepare-release.js
@@ -6,4 +6,4 @@ var gulp = require('gulp');
  * Run using "gulp prepare-release"
  * Runs clean, build, lint and test
  */
-gulp.task('prepare-release', ['clean', 'build', 'lint', 'test']);
+gulp.task('prepare-release', ['clean', 'build', 'jsdoc', 'lint', 'test']);
